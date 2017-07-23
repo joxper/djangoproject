384f6404ad6f6493256ccac18662e0c2340b63aa
jorgv <jorgv@DevLab.home>
2017-07-23 19:43:59 +0200

    django render basic functions

---
+++ restaurants/views.py
@@ -0,0 +1,16 @@
+import random
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+#	return HttpResponse("jello")
+	num = random.randint(0, 100000000)
+	list = [num, random.randint(0, 100000000), random.randint(0,1000000)]
+	context = {
+		"html_var": "false",
+		"num": num,
+		"list": list
+		}
+	return render(request, "base.html", context )
--- trydjango/settings/base.py
+++ trydjango/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '446r=tabk18cb2#uf57_qt2h1n^s)#$wfs@h$)dh^4$eink!1l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.35']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
