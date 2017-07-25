23954e396ed2ccf10217a88c0f536a2cb16ba3bf
joxper <joxper@gmail.com>
2017-07-25 10:54:34 +0200

    login

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, PasswordResetView
 
 from basics.views import HomeView, AboutView, AboutTemplateView
 
@@ -30,6 +31,11 @@ from restaurants.views import (
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view()),
+
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^password_reset/$', PasswordResetView.as_view(), name='password_reset'),
+
+
     url(r'^other/$', TemplateView.as_view(template_name='other.html')),
     url(r'^about/(?P<id>\d+)/$', AboutTemplateView.as_view()),
 
